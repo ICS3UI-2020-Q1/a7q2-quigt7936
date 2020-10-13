42c6b17641476f8cb903c0ca0afd37989e5c14e8
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-13 13:45:02 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Outputs the factors of a given number
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+  // outputs all the factors of the number
+  public static void factors(int factor) {
+    for (int i = 0; i < factor; i++) {
+      if ((factor % (i + 1)) == 0) {
+        System.out.println(i + 1);
+      }
+    }
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // gets the users number and runs the method
+    System.out.println("What number do you want the factors of?");
+    int factor = input.nextInt();
+    factors(factor);
     
   }
 }
